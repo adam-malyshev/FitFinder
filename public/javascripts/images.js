5194c4a312e5d2093395fb80012bbbfa34ef697f
Adam Malyshev <adammalyshev@adams-mbp-true.lan>
2020-06-07 04:54:06 -0400

    Auto-Color update, bug fixes

--- public/javascripts/images.js
+++ public/javascripts/images.js
@@ -2,7 +2,8 @@ const {Storage} = require('@google-cloud/storage');
 const storage = new Storage({keyFilename:process.env.GOOGLE_APPLICATION_CREDENTIALS});
 const bucket = storage.bucket(process.env.BUCKET);
 const Multer = require('Multer');
-
+const sharp = require('sharp');
+const path = require('path');
 
 function getPublicUrl(filename) {
   return `https://storage.googleapis.com/` + process.env.BUCKET + `/${filename}`;
@@ -38,6 +39,65 @@ function uploadImage(req, res, next) {
   stream.end(req.file.buffer);
 }
 
+async function crop(obj, cb){
+    var imageurl = obj.imgUrl;
+    var nameparse = imageurl.split('/');
+    var name = nameparse[nameparse.length - 1 ];
+    console.log("Name of the orig file: " , name);
+    var fileparse = imageurl.split('.');
+    var fileFormat = fileparse[fileparse.length - 1];
+    console.log("File Format:" , fileFormat);
+    var date = new Date();
+    var newname = obj.id + '_' + date.getTime() + '.' + fileFormat;
+    console.log("Name of the proc file: ", newname);
+
+    var readFile = bucket.file(name);
+    var writeFile =  bucket.file(newname);
+
+    var sx = obj.bounding_box.x0;
+    var sy = obj.bounding_box.y0;
+    var swidth = obj.bounding_box.x1 - obj.bounding_box.x0;
+    var sheight = obj.bounding_box.y1 - obj.bounding_box.y0;
+    var out;
+
+    // var dx = canvas.width / 2 - destwidth / 2;
+    // var dy = canvas.height / 2 - destheight / 2;
+
+    //crop image to bounding box using sharp
+
+
+    const cropImage =
+        sharp()
+            .extract({left: sx, top: sy, width: swidth, height: sheight})
+            .resize(100, 100, {
+                fit: sharp.fit.inside,
+                withoutEnlargement: true
+            })
+            .toFormat(fileFormat);
+
+    //create Read Stream
+    readFile.createReadStream()
+        .on('error', function(err) {})
+        .on('response', function(response) {
+            // Server connected and responded with the specified status and headers.
+            console.log("Connected");
+        })
+        .on('end', function() {
+            // The file is fully downloaded.
+            console.log("File read");
+        })
+        .pipe(cropImage)
+        .pipe(writeFile.createWriteStream())
+          .on('error', function(err) {})
+          .on('finish', async function() {
+            // The file upload is complete.
+            console.log("File uploaded");
+            await writeFile.makePublic();
+            out = getPublicUrl(newname);
+            console.log("URL after cropping:", out);
+            cb(out);
+          });
+}
 
 const multer = Multer({
   storage: Multer.MemoryStorage,
@@ -50,5 +110,6 @@ const multer = Multer({
 module.exports = {
   getPublicUrl,
   uploadImage,
+  crop,
   multer
 }
